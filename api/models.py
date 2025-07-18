65ab3a335151b18b656237406e4217027a64d17a
mohammad <mhmd.2831.mahdi@gmail.com>
2025-07-18 14:03:17 +0330

    add celery and first celery test

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ApiTarget(models.Model):
+    api = models.CharField(max_length=500)
+    interval_minutes = models.IntegerField(default=1)
+    def __str__(self):
+        return self.api
